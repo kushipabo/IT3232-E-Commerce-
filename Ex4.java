21b171351fd81c81bad61d3861eae268440ef24f
Pabodha_Wijesinghe <139679251+kushipabo@users.noreply.github.com>
2025-03-16 09:25:18 +0000

    Print odd numbers between 1 to 20

---
+++ Ex4.java
@@ -0,0 +1,8 @@
+public class Ex4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
